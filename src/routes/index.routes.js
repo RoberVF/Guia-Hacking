56c9d89e4be7314dd7132168cd1b154aeeb0dbee
Roberto Morales <robertomoralesfumero@gmail.com>
2024-02-10 14:15:50 +0000

    Cambios en ejs acitivities para hacer las tarjetas de manera comun

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,21 +1,65 @@
-const { Router }= require('express')
-const route= Router()
+const { Router } = require('express')
+const route = Router()
 
-route.get('/', (req, res)=>{
-    res.render('index.ejs')
+route.get('/', (req, res) => {
+    const activities = [
+        {
+            name: "Redes Wireless",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/redes",
+            image: "images/redes.jpg"
+        },
+        {
+            name: "Phising",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/",
+            image: "images/phishing.jpg"
+        },
+        {
+            name: "Hardening",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/",
+            image: "images/hardening.jpg"
+        },
+        {
+            name: "Information Gathering",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/information-gathering",
+            image: "images/igathering.jpg"
+        },
+        {
+            name: "Escaneo y análisis",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/",
+            image: "images/analisis.jpg"
+        },
+        {
+            name: "Explotación",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/",
+            image: "images/explotacion.jpeg"
+        },
+        {
+            name: "Post explotación",
+            description: "Hacking de redes Wireless. Se comprometera la seguridad de las claves WEP/WPA/WPA2. Se ejecutaran ataques de deautenticacion, ataques CTS, ataques EvilTwin y ataques a WPS entre otros",
+            url: "/",
+            image: "images/post.jpg"
+        }
+    ]
+    res.render('index.ejs', {activities})
 })
 
 //Tecnicas
-route.get('/redes', (req, res)=>{
+route.get('/redes', (req, res) => {
     res.render('pages/redes.ejs')
 })
-route.get('/information-gathering', (req, res)=>{
+route.get('/information-gathering', (req, res) => {
     res.render('pages/gathering.ejs')
 })
 
-route.get('/herramientas', (req,res)=>{
+route.get('/herramientas', (req, res) => {
     res.render('herramientas.ejs')
 })
 
-module.exports= route
+module.exports = route
 
